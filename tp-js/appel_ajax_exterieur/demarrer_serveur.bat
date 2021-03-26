65a471e42a5b5d5d928209f1709f887dca237a5f
didier <didier@d-defrance.fr>
2021-03-26 14:05:33 +0100

    s

---
+++ tp-js/appel_ajax_exterieur/demarrer_serveur.bat
@@ -0,0 +1,9 @@
+REM npm install -g lite-server
+lite-server
+REM lite-server prend en charge automatiquement
+REM tous les fichiers .html , .css , .js du répertoire
+REM courant et des sous répertoire
+
+REM 3000 est le numero de port de lite-server
+REM url par defaut : http://localhost:3000/index.html
+REM url  : http://localhost:3000/ajax_prod.html
