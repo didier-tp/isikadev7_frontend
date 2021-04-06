7a1ae0a15ee5d1c4706f179e014702255f1ce3b1
didier <didier@d-defrance.fr>
2021-04-06 16:37:30 +0200

    s

--- tp-js/my-app/src/app/header/header.component.ts
+++ tp-js/my-app/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MenuDefinition } from 'src/bs-util/data/MenuDefinition';
 import { PreferencesService } from '../common/service/preferences.service';
 
 @Component({
@@ -12,7 +13,19 @@ export class HeaderComponent implements OnInit {
   public titre :string ="default-title";
    //à afficher via {{titre}} dans header.component.html
 
-
+   myMenuDef : MenuDefinition[] = [
+    { label : "basic" , path : "/ngr-basic" } , 
+    { label : "welcome" , path : "/ngr-welcome" },
+    { label : "autres" , 
+      children : [
+        { label : "login" , path : "/ngr-login" } ,
+        { label : "welcome" , path : "/ngr-welcome" },
+        { divider : true },
+        { label : "menu-item3" , path : "path3" }
+      ]
+    }
+    
+    ];
 
  
   constructor(public preferencesService : PreferencesService) { }
