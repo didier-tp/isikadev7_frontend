c7e655872c93cb9c941bdc57fec68d758cdcd0b6
didier <didier@d-defrance.fr>
2021-04-06 09:25:04 +0200

    mardi 6 avril matin

---
+++ tp-js/my-app/src/app/basic/calculatrice/calculatrice.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-calculatrice',
+  templateUrl: './calculatrice.component.html',
+  styleUrls: ['./calculatrice.component.scss']
+})
+export class CalculatriceComponent implements OnInit {
+
+  a! : number ;
+  b! : number ;
+  res! : number ;
+
+  montrerHisto : boolean = true;
+  historiqueCalculs :string[] = [];
+
+  onCalculer(op:string){
+       switch(op){
+         case "+" :
+            this.res = Number(this.a) + Number(this.b);  break;
+        case "-" :
+              this.res = Number(this.a)- Number(this.b);  break;
+        case "*" :
+            this.res = Number(this.a) * Number(this.b);  break;
+        default:
+            this.res = 0;
+       }
+       this.historiqueCalculs.push(`${this.a} ${op} ${this.b} = ${this.res}`)
+  }
+
+  //coordonnées relatives de la souris qui survole une div
+  x! :number; 
+  y! :number;
+
+  onMouseMove(evt : MouseEvent){
+    let currentDiv : HTMLElement  = <HTMLElement> evt.target;
+    this.x = evt.pageX - currentDiv.offsetLeft;
+    this.y = evt.pageY - currentDiv.offsetTop;
+  }
+
+  onMouseLeave(evt : MouseEvent){
+    this.x=0; this.y=0;
+  }
+
+  constructor() { 
+  }
+
+  ngOnInit(): void {
+  }
+
+}
